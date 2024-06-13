ed619bc40d79ddf5af5f0838fdec98c45fd97e47
Timo <timetais22@gmail.com>
2024-06-13 15:40:09 +0200

    formating planning message

--- src/commands/planning.js
+++ src/commands/planning.js
@@ -61,8 +61,8 @@ module.exports = {
 			switch (spanType) {
 			case 'lblDay':
 				// Jour
-				if (message.length !== '>>> ') { message += '\n\n'; }
-				message += `**__${textContent.toUpperCase()}__**`;
+				if (message.length !== '>>> ') { message += '\n'; }
+				message += `## **__${textContent.toUpperCase()}__**`;
 				break;
 			case 'lblEvtRange':
 				// Heure
